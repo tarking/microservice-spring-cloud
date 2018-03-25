44764d2687a6ef3aac0e31c5b6ea2b2ef633c0b8
tarking <492300925@qq.com>
2018-03-25 23:06:25 +0800

    将服务注册到eureka上
    1.给eureka增加用户名密码，访问时需要输入指定的用户名密码，注册到eureka时需要在defaultZone中配置应的用户名、密码；
    2.格式化instance-id
    
    Co-Authored-By: tarking <492300925@qq.com>

--- microservice-gateway/src/main/java/com/tarking/microservice/GatewayApplication.java
+++ microservice-gateway/src/main/java/com/tarking/microservice/GatewayApplication.java
@@ -2,10 +2,12 @@ package com.tarking.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient   //为什么不使用该注解，同样可以注册到eureka
 public class GatewayApplication {
 
   public static void main(String[] args) {
